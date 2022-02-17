67af45f17df54b5ce9af1753d334a387a2590ab1
Avishek Mallik <avishekmallik@hotmail.com>
2022-02-17 23:59:27 +0000

    new

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,13 +7,11 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+select * from FOODEXPRESS.public.FOODEXPRESS_INFO limit 1
 
 )
 
@@ -25,3 +23,4 @@ from source_data
 */
 
 -- where id is not null
+
